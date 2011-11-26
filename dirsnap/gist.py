e6e62e24c2db6dd3ff07c39e3f369b285552a76a
Kenneth Reitz <me@kennethreitz.com>
2011-11-26 13:01:22 -0500

    gist upload

--- dirsnap/gist.py
+++ dirsnap/gist.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 
-import json
+"""
+dirsnap.gist
+~~~~~~~~~~~~
+
+Uploads the given file to Gist, anonomously.
+"""
+
+import os
 
 import requests
 
+GIST_URL = 'https://gist.github.com/gists'
+GIST_TEMPLATE = 'https://gist.github.com/raw/{0}/{1}'
 
-url = 'https://api.github.com/gists'
 
+def upload(path):
+    """Uploads the file at the given path to gist.github.com."""
 
-payload = {
-    'description': 'dirsnap catpured TIMESTAMP',
-    'public': False,
-    'files': {
-        'filename': {
-            'content': 'blah'
-        }
+    filename = path.split(os.path.sep)[-1]
+
+    with open(path, 'rb') as f:
+        content = f.read()
+
+    payload = {
+       'file_ext[gistfile#0]': '.txt',
+       'file_name[gistfile#0]': filename,
+       'file_contents[gistfile#0]': content
     }
-}
-payload = json.dumps(payload)
 
-r = requests.post(url, data=payload)
-print r
-print r.headers
+    # Send it up to GitHub.
+    r = requests.post(GIST_URL, data=payload, allow_redirects=True)
+
+    gist_id = r.url.split(os.path.sep)[-1]
+
+    return GIST_TEMPLATE.format(gist_id, filename)
