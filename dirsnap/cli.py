9c1996bffa3dfe526ac371ffe3231ba6c367645a
Kenneth Reitz <me@kennethreitz.com>
2011-11-26 13:01:27 -0500

    cli interface

--- dirsnap/cli.py
+++ dirsnap/cli.py
@@ -9,6 +9,37 @@ The CLI for Dirsnap.
 
 import sys
 
+from clint import args
+
+from .archive import archive
+from .gist import upload
+
+
+
+def display_usage():
+    """Obvious, no?"""
+    print 'Usage: dirsnap <dir>'
+
+
 def main():
-    print '\o/'
+    """Dirsnap entrypoint."""
+
+    paths = args.files
+
+    if not paths:
+        display_usage()
+        sys.exit(1)
+
+    # Create temp archive.
+    print >> sys.stderr, 'Creating snapshot...'
+    path = archive(paths)
+
+    # Upload temp archive to service.
+    print >> sys.stderr, 'Uploading snapshot...'
+    url = upload(path)
+    print >> sys.stderr, 'Done! Here it is:'
+
+    print url
+
+
     sys.exit()
